6ae3df1bb814289475a41fc4bb09410b3572740f
fran-silesm <63246984+fran-silesm@users.noreply.github.com>
2021-11-25 18:55:48 -0300

    atualizando funcao e variavel desnecessaria

--- laboratorio10/ex02.cpp
+++ laboratorio10/ex02.cpp
@@ -5,21 +5,20 @@
 #include <vector>
 using namespace std;
 
-void set_valor(int e, int v){
+void set_valor(int v){
   if (v > 9 || v < 0)
     throw std::out_of_range("Erro: Numero fora dos limites.");
 
-  e = v;
   cout << "Este e um numero simples, no intervalo ideal.\n";
 }
 
 int main() {
-  int num, elem;
+  int num;
 
   try {
     cout << "\nInsira um numero entre 0 e 9: ";
     cin >> num;
-    set_valor(elem, num);
+    set_valor(num);
   }catch (out_of_range &ex){
     cout << ex.what() << "\n";
   }
